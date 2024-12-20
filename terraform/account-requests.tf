5e5c9a3817cb0a9416475d72e7decc5d257040f6
Carlos Enriquez <cae@amazon.com>
2024-12-20 16:22:50 -0500

    first commit

--- terraform/account-requests.tf
+++ terraform/account-requests.tf
@@ -1,35 +1,35 @@
-module "account_request_01" {
-  source = "./modules/aft-account-request"
+# module "account_request_01" {
+#   source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "cae+lab01-audit@amazon.com"
-    AccountName               = "Audit"
-    ManagedOrganizationalUnit = "Security" 
-    SSOUserEmail              = "cae+lab01-audit@amazon.com"
-    SSOUserFirstName          = "Audit"
-    SSOUserLastName           = "Security"
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "cae+lab01-audit@amazon.com"
+#     AccountName               = "Audit"
+#     ManagedOrganizationalUnit = "Security" 
+#     SSOUserEmail              = "cae+lab01-audit@amazon.com"
+#     SSOUserFirstName          = "Audit"
+#     SSOUserLastName           = "Security"
+#   }
 
-  account_tags = {
-    "ABC:Owner"       = "cae@amazon.com.com"
-    "ABC:Division"    = "ENT"
-    "ABC:Environment" = "Dev"
-    "ABC:CostCenter"  = "123456"
-    "ABC:Vended"      = "true"
-    "ABC:DivCode"     = "102"
-    "ABC:BUCode"      = "ABC003"
-    "ABC:Project"     = "123456"
-  }
+#   account_tags = {
+#     "ABC:Owner"       = "cae@amazon.com.com"
+#     "ABC:Division"    = "ENT"
+#     "ABC:Environment" = "Dev"
+#     "ABC:CostCenter"  = "123456"
+#     "ABC:Vended"      = "true"
+#     "ABC:DivCode"     = "102"
+#     "ABC:BUCode"      = "ABC003"
+#     "ABC:Project"     = "123456"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "AWS Control Tower Lab"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
-  }
+#   change_management_parameters = {
+#     change_requested_by = "AWS Control Tower Lab"
+#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+#   }
 
-  custom_fields = {
-    custom1 = "a"
-    custom2 = "b"
-  }
+#   custom_fields = {
+#     custom1 = "a"
+#     custom2 = "b"
+#   }
 
-  account_customizations_name = "Audit"
-}
+#   account_customizations_name = "Audit"
+# }
