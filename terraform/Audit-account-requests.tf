24880a8021f8cee469272b7ebd1f1a86c4b85f07
Carlos Enriquez <cae@amazon.com>
2025-01-15 13:05:40 -0500

    management 4

--- terraform/Audit-account-requests.tf
+++ terraform/Audit-account-requests.tf
@@ -33,38 +33,38 @@ module "account_request_01" {
 
   account_customizations_name = "Audit"
 }
-# module "account_request_02" {
-#   source = "./modules/aft-account-request"
+module "account_request_02" {
+  source = "./modules/aft-account-request"
 
-#   control_tower_parameters = {
-#     AccountEmail              = "cae+mn+lab01@amazon.com"
-#     AccountName               = "Management"
-#     ManagedOrganizationalUnit = "Root" 
-#     SSOUserEmail              = "cae@amazon.com"
-#     SSOUserFirstName          = "Carlos"
-#     SSOUserLastName           = "Enriquez"
-#   }
+  control_tower_parameters = {
+    AccountEmail              = "cae+mn+lab01@amazon.com"
+    AccountName               = "Management"
+    ManagedOrganizationalUnit = "Root" 
+    SSOUserEmail              = "cae@amazon.com"
+    SSOUserFirstName          = "Carlos"
+    SSOUserLastName           = "Enriquez"
+  }
 
-#   account_tags = {
-#     "ABC:Owner"       = "cae@amazon.com.com"
-#     "ABC:Division"    = "ENT"
-#     "ABC:Environment" = "Dev"
-#     "ABC:CostCenter"  = "123456"
-#     "ABC:Vended"      = "true"
-#     "ABC:DivCode"     = "102"
-#     "ABC:BUCode"      = "ABC003"
-#     "ABC:Project"     = "123456"
-#   }
+  account_tags = {
+    "ABC:Owner"       = "cae@amazon.com.com"
+    "ABC:Division"    = "ENT"
+    "ABC:Environment" = "Dev"
+    "ABC:CostCenter"  = "123456"
+    "ABC:Vended"      = "true"
+    "ABC:DivCode"     = "102"
+    "ABC:BUCode"      = "ABC003"
+    "ABC:Project"     = "123456"
+  }
 
-#   change_management_parameters = {
-#     change_requested_by = "AWS Control Tower Lab"
-#     change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
-#   }
+  change_management_parameters = {
+    change_requested_by = "AWS Control Tower Lab"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform (AFT)"
+  }
 
-#   custom_fields = {
-#     custom1 = "a"
-#     custom2 = "b"
-#   }
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
 
-#   account_customizations_name = "Management"
-# }
+  account_customizations_name = "Management"
+}
